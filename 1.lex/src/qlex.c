11ff3624e1a76e8b163b448960080ed965877cb4
zoanana990 <harry021633@gmail.com>
2024-03-01 00:13:39 +0800

    Add the lexer and parse num, str and identifier function

--- 1.lex/src/qlex.c
+++ 1.lex/src/qlex.c
@@ -4,6 +4,8 @@
 #include <qstr.h>
 #include <qcc.h>
 
+#include <color.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,12 +17,14 @@ token_t     *ptr_token_hashtable[MAX_KEY];
 char         fch;
 
 qstr_t       token_string;
+qstr_t       source_string;
+int          token_value;
 
 extern FILE *fin;
 
 /* check the char is number */
 int is_digit(char c) {
-    return (c >= '0' && c <= '9');
+    return c >= '0' && c <= '9';
 }
 
 int is_alpha(char c) {
@@ -35,7 +39,7 @@ int is_alpha_or_underline(char c) {
 /* check the char is alphabet, number or underline */
 int is_alnum(char c) {
     return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
-            (c >= '0' && c <= '9') || (c == '_'));
+            (c >= '0' && c <= '9') || c == '_');
 }
 
 /**
@@ -54,6 +58,9 @@ int elf_hash(char *key) {
     return h % MAX_KEY;
 }
 
+/***
+ * TODO: we can replace `calloc` with this
+ * */
 void *mallocz(int size) {
     void *ptr;
     ptr = malloc(size);
@@ -127,9 +134,111 @@ token_t *parse_identifier(void) {
         qstr_chcat(&token_string, fch);
         getch();
     }
-    
+
     qstr_chcat(&token_string, '\0');
+
     return token_insert(token_string.data);
+}
+
+void parse_num() {
+/**
+ * Here we can parse floating point and later we
+ * will implement the hex and other base value
+ */
+    qstr_reset(&token_string);
+    qstr_reset(&source_string);
+
+    do {
+        qstr_chcat(&token_string, fch);
+        qstr_chcat(&source_string, fch);
+        getch();
+    } while(is_digit(fch));
+
+    if(fch == '.') {
+        do {
+            qstr_chcat(&token_string, fch);
+            qstr_chcat(&source_string, fch);
+            getch();
+        } while(is_digit(fch));
+    }
+
+    qstr_chcat(&token_string, '\0');
+    qstr_chcat(&source_string, '\0');
+
+    token_value = atoi(token_string.data);
+}
+
+/**
+ *
+ * */
+void parse_string(char sep) {
+    char c;
+
+    qstr_reset(&token_string);
+    qstr_reset(&source_string);
+
+    qstr_chcat(&source_string, sep);
+    getch();
+
+    while(1) {
+        if(fch == sep)
+            break;
+        else if(fch == '\\') {
+            qstr_chcat(&source_string, fch);
+            getch();
+            switch(fch) {
+                case '0':
+                    c = '\0';
+                    break;
+                case 'a':
+                    c = '\a';
+                    break;
+                case 'b':
+                    c = '\b';
+                    break;
+                case 't':
+                    c = '\t';
+                    break;
+                case 'n':
+                    c = '\n';
+                    break;
+                case 'v':
+                    c = '\v';
+                    break;
+                case 'r':
+                    c = '\r';
+                    break;
+                case 'f':
+                    c = '\f';
+                    break;
+                case '\\':
+                    c = '\\';
+                    break;
+                case '\'':
+                    c = '\'';
+                    break;
+                case '\"':
+                    c = '\"';
+                    break;
+                default:
+                    c = fch;
+                    break;
+            }
+            qstr_chcat(&token_string, c);
+            qstr_chcat(&source_string, fch);
+            getch();
+        } else {
+            qstr_chcat(&token_string, fch);
+            qstr_chcat(&source_string, fch);
+            getch();
+        }
+    }
+
+    qstr_chcat(&token_string, '\0');
+    qstr_chcat(&source_string, sep);
+    qstr_chcat(&source_string, '\0');
+
+    getch();
 }
 
 token_t* token_direct_insert(token_t *tp) {
@@ -152,12 +261,22 @@ void init_lex() {
 
     static token_t keywords[] = {
         {TOKEN_PLUS,            NULL, "+",            NULL, NULL},
+        {TOKEN_PLUS_EQ,         NULL, "+=",           NULL, NULL},
         {TOKEN_MINUS,           NULL, "-",            NULL, NULL},
+        {TOKEN_MINUS_EQ,        NULL, "-=",           NULL, NULL},
         {TOKEN_STAR,            NULL, "*",            NULL, NULL},
+        {TOKEN_MULTI_EQ,        NULL, "*=",            NULL, NULL},
         {TOKEN_DIVIDE,          NULL, "/",            NULL, NULL},
+        {TOKEN_DIVIDE_EQ,       NULL, "/=",           NULL, NULL},
         {TOKEN_MOD,             NULL, "%",            NULL, NULL},
+        {TOKEN_MOD_EQ,          NULL, "%=",            NULL, NULL},
         {TOKEN_EQ,              NULL, "==",           NULL, NULL},
+        {TOKEN_NOT,             NULL, "!",            NULL, NULL},
         {TOKEN_NEQ,             NULL, "!=",           NULL, NULL},
+        {TOKEN_SHIFT_LEFT,      NULL, "<<",           NULL, NULL},
+        {TOKEN_SHIFT_LEFT_EQ,   NULL, "<<=",           NULL, NULL},
+        {TOKEN_SHIFT_RIGHT,     NULL, ">>",           NULL, NULL},
+        {TOKEN_SHIFT_RIGHT_EQ,  NULL, ">>=",           NULL, NULL},
         {TOKEN_LT,              NULL, "<",            NULL, NULL},
         {TOKEN_LEQ,             NULL, "<=",           NULL, NULL},
         {TOKEN_GT,              NULL, ">",            NULL, NULL},
@@ -166,8 +285,10 @@ void init_lex() {
         {TOKEN_POINTTO,         NULL, "->",           NULL, NULL},
         {TOKEN_DOT,             NULL, ".",            NULL, NULL},
         {TOKEN_BAND,            NULL, "&",            NULL, NULL},
+        {TOKEN_BAND_EQ,         NULL, "&=",           NULL, NULL},
         {TOKEN_AND,             NULL, "&&",           NULL, NULL},
         {TOKEN_BOR,             NULL, "|",            NULL, NULL},
+        {TOKEN_BOR_EQ,          NULL, "|=",           NULL, NULL},
         {TOKEN_OR,              NULL, "||",           NULL, NULL},
         {TOKEN_OPEN_PARENTH,    NULL, "(",            NULL, NULL},
         {TOKEN_CLOSE_PARENTH,   NULL, ")",            NULL, NULL},
@@ -179,7 +300,7 @@ void init_lex() {
         {TOKEN_COMMA,           NULL, ",",            NULL, NULL},
         {TOKEN_COLON,           NULL, ":",            NULL, NULL},
         {TOKEN_ELLIPSIS,        NULL, "...",          NULL, NULL},
-        {TOKEN_EOF,             NULL, "end of file",  NULL, NULL},
+        {TOKEN_EOF,             NULL, "\n\n",         NULL, NULL},
         {TOKEN_CINT,            NULL, "int const",    NULL, NULL},
         {TOKEN_CCHAR,           NULL, "char const",   NULL, NULL},
         {TOKEN_CSTR,            NULL, "string const", NULL, NULL},
@@ -195,9 +316,11 @@ void init_lex() {
         {TOKEN_KEY_BREAK,       NULL, "break",        NULL, NULL},
         {TOKEN_KEY_RETURN,      NULL, "return",       NULL, NULL},
         {TOKEN_KEY_SIZEOF,      NULL, "sizeof",       NULL, NULL},
+#ifdef _WIN32
         {TOKEN_KEY_ALIGN,       NULL, "__align",      NULL, NULL},
         {TOKEN_KEY_CDECL,       NULL, "__cdcel",      NULL, NULL},
         {TOKEN_KEY_STDCALL,     NULL, "__stdcall",    NULL, NULL},
+#endif
         {0,                     NULL, NULL,           NULL, NULL},
     };
     qarr_init(&token_table, 8);
@@ -225,7 +348,7 @@ void getch() {
  * macOS:   \r   to get the newline
  */
 void skip_white_space() {
-    while((fch = ' ') || (fch = '\t') || (fch = '\r')) {
+    while((fch == ' ') || (fch == '\t') || (fch == '\r') || (fch == '\n')) {
 #if defined (_WIN32)
         if(fch == '\r') {
             getch();
@@ -233,12 +356,8 @@ void skip_white_space() {
                 return;
             line_num++;
         }
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__APPLE__)
         if(fch == '\n') {
-            line_num++;
-        }
-#elif defined(__APPLE__)
-        if(fch == '\r') {
             line_num++;
         }
 #endif
@@ -278,12 +397,17 @@ void parse_comment() {
     } while(1);
 }
 
+void preprocessor() {
+    while(fch != '\n' && fch != EOF)
+        getch();
+}
+
 /* function name: word_extract
  * note: ignore the comments and delimiters
  */
 void word_extract() {
     while(1) {
-        if((fch == ' ') || (fch == '\t') || (fch == '\r'))
+        if((fch == ' ') || (fch == '\t') || (fch == '\r') || (fch == '\n'))
             skip_white_space();
         else if(fch == '/') {
             getch();
@@ -324,20 +448,219 @@ void get_token() {
             token = tp->token_code;
             break;
         }
-
+        case '0': case '1': case '2': case '3':
+        case '4': case '5': case '6': case '7':
+        case '8': case '9':
+            parse_num();
+            token = TOKEN_CINT;
+            break;
+        case '+':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_PLUS_EQ;
+                getch();
+            } else
+                token = TOKEN_PLUS;
+            break;
+        case '-':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_MINUS_EQ;
+                getch();
+            } else if (fch == '>') {
+                token = TOKEN_POINTTO;
+                getch();
+            } else
+                token = TOKEN_MINUS;
+            break;
+        case '*':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_MULTI_EQ;
+                getch();
+            } else
+                token = TOKEN_STAR;
+            break;
+        case '/':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_DIVIDE_EQ;
+                getch();
+            } else
+                token = TOKEN_DIVIDE;
+            break;
+        case '%':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_MOD_EQ;
+                getch();
+            } else
+                token = TOKEN_MOD;
+            break;
+        case '=':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_EQ;
+                getch();
+            } else
+                token = TOKEN_ASSIGN;
+            break;
+        case '!':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_NEQ;
+                getch();
+            } else
+                token = TOKEN_NOT;
+            break;
+        case '<':
+            getch();
+            if(fch == '<') {
+                getch();
+                if(fch == '=') {
+                    token = TOKEN_SHIFT_LEFT_EQ;
+                    getch();
+                } else
+                    token = TOKEN_SHIFT_LEFT;
+            } else if(fch == '=') {
+                token = TOKEN_LEQ;
+                getch();
+            } else
+                token = TOKEN_LT;
+            break;
+        case '>':
+            getch();
+            if(fch == '>') {
+                getch();
+                if(fch == '=') {
+                    token = TOKEN_SHIFT_RIGHT_EQ;
+                    getch();
+                } else
+                    token = TOKEN_SHIFT_RIGHT;
+            } else if(fch == '=') {
+                token = TOKEN_GEQ;
+                getch();
+            } else
+                token = TOKEN_GT;
+            break;
+        case '.':
+            getch();
+            if(fch == '.') {
+                getch();
+                if(fch == '.') {
+                    token = TOKEN_ELLIPSIS;
+                    getch();
+                }
+                else
+                    error("It is not ellipsis\n");
+            } else {
+                token = TOKEN_DOT;
+            }
+            break;
+        case '&':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_BAND_EQ;
+                getch();
+            } else if(fch == '&') {
+                token = TOKEN_AND;
+                getch();
+            }else
+                token = TOKEN_BAND;
+            break;
+        case '|':
+            getch();
+            if(fch == '=') {
+                token = TOKEN_BOR_EQ;
+                getch();
+            } else if(fch == '|') {
+                token = TOKEN_OR;
+                getch();
+            }else
+                token = TOKEN_BOR;
+            break;
+        case '(':
+            token = TOKEN_OPEN_PARENTH;
+            getch();
+            break;
+        case ')':
+            token = TOKEN_CLOSE_PARENTH;
+            getch();
+            break;
+        case '[':
+            token = TOKEN_OPEN_BRACKET;
+            getch();
+            break;
+        case ']':
+            token = TOKEN_CLOSE_BRACKET;
+            getch();
+            break;
+        case '{':
+            token = TOKEN_OPEN_CURLY;
+            getch();
+            break;
+        case '}':
+            token = TOKEN_CLOSE_CURLY;
+            getch();
+            break;
+        case ',':
+            token = TOKEN_COMMA;
+            getch();
+            break;
+        case ';':
+            token = TOKEN_SEMICOLON;
+            getch();
+            break;
+        case ':':
+            token = TOKEN_COLON;
+            getch();
+            break;
+        case '#':
+            /* do nothing here */
+            preprocessor();
+            break;
+        case '\'':
+            parse_string(fch);
+            token = TOKEN_CCHAR;
+            token_value = *(char *)token_string.data;
+            break;
+        case '\"':
+            parse_string(fch);
+            token = TOKEN_CSTR;
+            break;
         case EOF:
-            printf("EOF\n");
             token = TOKEN_EOF;
             break;
         default:
-            error("cannot recognize character %c\n", fch);
+            error("cannot recognize character \"%c\"\n", fch);
             getch();
             break;
     }
 }
 
+char *get_token_string(int str_num) {
+    
+    if(str_num > token_table.count)
+        return NULL;
+    else if(str_num >= TOKEN_CINT && str_num <= TOKEN_CSTR)
+        return source_string.data;
+    else
+        /* find the table we create at lex_init */
+        return ((token_t *)token_table.data[str_num])->spelling;
+}
+
 void syntax_on() {
 
+    char *ptr_c = get_token_string(token);
+
+    if(token >= TOKEN_KEY_IDENT)
+        pr_identifier("%s", ptr_c);
+    else if(token >= TOKEN_KEY_CHAR)
+        pr_keyword("%s", ptr_c);
+    else if(token >= TOKEN_CINT)
+        pr_constant("%s", ptr_c);
+    else
+        pr_token("%s", ptr_c);
 }
 
 void lexical_coloring() {
@@ -347,7 +670,7 @@ void lexical_coloring() {
     do {
         get_token();
         syntax_on();
-        break;
+        // break;
     } while(token != TOKEN_EOF);
 
     printf("Lines of code: %d\n", line_num);
