2c38b9f431a519bc0efc2da3c07146329b8a2a85
Hsin-Yu.Chen <harry021633@gmail.com>
2024-03-25 22:27:58 +0800

    Implement a new method

--- 3.symbol/example/basic.c
+++
@@ -1,4 +0,0 @@
-int main();
-int main() {
-    return 0;
-}
--- 3.symbol/inc/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UTILS_H__
-#define __UTILS_H__
-
-#define MAX_KEY                  (1024)
-#define GET_ALIGNMENT(size)      ((size_t)1 << (64 - __builtin_clzll((size_t)(size) - 1)))
-#define calc_align(n, align)     (((n) + (align) - 1) & (~((align) - 1)))
-
-#endif
